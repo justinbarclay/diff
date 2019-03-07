baf3443e75db00d1f8443f808abd3e3feff672b7
Justin Barclay <justincbarclay@gmail.com>
2019-03-07 01:05:59 -0700

    Fix bug where new lines were being added to end of files

--- src/diff_file.rs
+++ src/diff_file.rs
@@ -47,8 +47,8 @@ pub fn diff_files(file_one: &str, file_two: &str)-> Result<Vec<(i32, HashMap<Str
 // Goes through each line in file_one and file_two and applies decoration to the insertions and deletions in each line
 pub fn differences_by_line(file_one: &str, file_two: &str, edits: Vec<(i32, HashMap<String, Vec<Edit>>)>) -> String {
 
-  let file_one_lines: Vec<&str> = file_one.split('\n').collect();
-  let file_two_lines: Vec<&str> = file_two.split('\n').collect();
+  let file_one_lines: Vec<&str> = file_one.split_terminator('\n').collect();
+  let file_two_lines: Vec<&str> = file_two.split_terminator('\n').collect();
 
   let combined_lines = file_one_lines.iter().zip(file_two_lines.iter()).zip(edits.iter());
   let mut result = String::new();
